96bd4ef9a00ea0bbad279aedda032083c03bf02c
Yadpreet Cheema <yadpreetcheema99@gmail.com>
2025-05-08 00:04:45 -0700

    changes

--- server/src/routes/scoreRoutes.ts
+++ server/src/routes/scoreRoutes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { getBarChartScores } from '../controllers/getBarChartScores';
-import { getAllScores } from '../controllers/getAllScores';
+import { getAllScores } from '../controllers/getAllScoresController';
 import { postScore } from '../controllers/postScores';
 
 const router = Router();
